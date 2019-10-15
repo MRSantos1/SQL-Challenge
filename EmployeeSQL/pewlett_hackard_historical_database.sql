9f08d302eb238348881cd89aa69776c929c3a29b
MRSantos1 <martin.r.santos.1@outlook.com>
2019-10-15 10:33:40 -0700

    adding ERD Diagram

--- EmployeeSQL/pewlett_hackard_historical_database.sql
+++ EmployeeSQL/pewlett_hackard_historical_database.sql
@@ -143,7 +143,22 @@ LEFT JOIN dept_emp d
 ON e.emp_no = d.emp_no
 LEFT JOIN departments
 ON d.dept_no = departments.dept_no
-WHERE departments.dept_name lIKE 'Sales';
+WHERE departments.dept_name lIKE 'Sales'
+
+-- Displaying all employees by: Sales Dept, Development Dept employee no, first, last name, and dept name
+
+Select
+e.emp_no,
+first_name,
+last_name,
+dept_name
+FROM employees e
+LEFT JOIN dept_emp d
+ON e.emp_no = d.emp_no
+LEFT JOIN departments
+ON d.dept_no = departments.dept_no
+WHERE departments.dept_name lIKE 'Sales'
+OR departments.dept_name LIKE 'Development';
 
 -- Displaying last name frequency
 
